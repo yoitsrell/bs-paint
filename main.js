9254306ba60048572ee5bbd4790cd19fd1c93f0d
Tyrell Cotton <tyrell.cotton@codeimmersives.com>
2020-03-19 11:00:17 -0400

    Having trouble getting brush to switch

---
+++ main.js
@@ -0,0 +1,36 @@
+const colorOne = document.querySelector(".palette-color color-1")
+const colorTwo = document.querySelector(".palette-color color-2")
+const colorThree = document.querySelector(".palette-color color-3")
+const colorFour = document.querySelector(".palette-color color-4")
+const colorFive = document.querySelector(".palette-color color-5")
+
+function brushPaintSwap1(){
+    let swapColor = document.querySelector(".current-brush color-1")
+    swapColor.classList.replace(swapColor.classList[1], "color-1")
+}
+
+function brushPaintSwap2(){
+    let swapColor = document.querySelector(".current-brush color-1")
+    swapColor.classList.replace(swapColor.classList[1], "color-2")
+}
+
+function brushPaintSwap3(){
+    let swapColor = document.querySelector(".current-brush color-1")
+    swapColor.classList.replace(swapColor.classList[1], "color-3")
+}
+
+function brushPaintSwap4(){
+    let swapColor = document.querySelector(".current-brush color-1")
+    swapColor.classList.replace(swapColor.classList[1], "color-4")
+}
+
+function brushPaintSwap5(){
+    let swapColor = document.querySelector(".current-brush color-1")
+    swapColor.classList.replace(swapColor.classList[1], "color-5")
+}
+
+colorOne.addEventListener("click", brushPaintSwap1)
+colorTwo.addEventListener('click', brushPaintSwap2)
+colorThree.addEventListener('click', brushPaintSwap3)
+colorFour.addEventListener('click', brushPaintSwap4)
+colorFive.addEventListener('click', brushPaintSwap5)
