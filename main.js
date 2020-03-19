aaef0700670c1a5cfca8c145793fbb9200e3fc4e
Tyrell Cotton <tyrell.cotton@codeimmersives.com>
2020-03-19 11:39:07 -0400

    Changed color-5 to white

--- main.js
+++ main.js
@@ -4,33 +4,34 @@ const colorThree = document.querySelector(".palette-color color-3")
 const colorFour = document.querySelector(".palette-color color-4")
 const colorFive = document.querySelector(".palette-color color-5")
 
-function brushPaintSwap1(){
-    let swapColor = document.querySelector(".current-brush color-1")
+const  brushPaintSwap1 = function(){
+    let swapColor = document.querySelector(".current-brush");
     swapColor.classList.replace(swapColor.classList[1], "color-1")
 }
 
-function brushPaintSwap2(){
-    let swapColor = document.querySelector(".current-brush color-1")
+const brushPaintSwap2 = function(){
+    let swapColor = document.querySelector(".current-brush");
     swapColor.classList.replace(swapColor.classList[1], "color-2")
 }
 
 function brushPaintSwap3(){
-    let swapColor = document.querySelector(".current-brush color-1")
+    let swapColor = document.querySelector(".current-brush");
     swapColor.classList.replace(swapColor.classList[1], "color-3")
 }
 
 function brushPaintSwap4(){
-    let swapColor = document.querySelector(".current-brush color-1")
+    let swapColor = document.querySelector(".current-brush");
     swapColor.classList.replace(swapColor.classList[1], "color-4")
 }
 
 function brushPaintSwap5(){
-    let swapColor = document.querySelector(".current-brush color-1")
+    let swapColor = document.querySelector(".current-brush");
     swapColor.classList.replace(swapColor.classList[1], "color-5")
 }
 
-colorOne.addEventListener("click", brushPaintSwap1)
+colorOne.addEventListener('click', brushPaintSwap1)
 colorTwo.addEventListener('click', brushPaintSwap2)
 colorThree.addEventListener('click', brushPaintSwap3)
 colorFour.addEventListener('click', brushPaintSwap4)
 colorFive.addEventListener('click', brushPaintSwap5)
+
